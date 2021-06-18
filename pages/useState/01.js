0662fba6990a77a47888115fb7a5efd3e7bf6c7a
Neeraj Dalal <admin@nrjdalal.com>
2021-06-18 10:46:03 +0530

    Fri Jun 18 10:46:03 IST 2021

--- pages/useState/01.js
+++ pages/useState/01.js
@@ -40,7 +40,7 @@ const Snippet = () => {
       <div className="flex flex-col items-center">
         <h4>{text}</h4>
         <button className="mt-4 text-white" onClick={handleClick}>
-          button text
+          button
         </button>
       </div>
     </>
