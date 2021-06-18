7a8e4047c9c3d55399753d1097c88c9129662042
Neeraj Dalal <admin@nrjdalal.com>
2021-06-18 11:22:48 +0530

    Fri Jun 18 11:22:48 IST 2021

--- pages/index.js
+++ pages/index.js
@@ -7,19 +7,19 @@ const index = () => {
         <h4 className="pb-4">useState</h4>
 
         <Link href="/useState/01">
-          <a className="pb-2">change the text field</a>
+          <a>change the text field</a>
         </Link>
         <br />
         <Link href="/useState/02">
-          <a className="pb-2">add or remove the array items</a>
+          <a>add or remove the array items</a>
         </Link>
         <br />
         <Link href="/useState/03">
-          <a className="pb-2">change property of an object</a>
+          <a>change property of an object</a>
         </Link>
         <br />
         <Link href="/useState/04">
-          <a className="pb-2">implement basic / timeout based counter</a>
+          <a>implement basic / timeout based counter</a>
         </Link>
         <br />
       </div>
